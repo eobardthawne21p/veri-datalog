636e1d6533edc088df89d5fbdc6ed414435660d0
Paul Hitchcox <eobardthawne21p@gmail.com>
2024-06-21 11:39:50 -0400

    working on deep_view issue

--- verus/lcf/src/verusport.rs
+++ verus/lcf/src/verusport.rs
@@ -17,6 +17,9 @@
 //#[allow(inconsistent_fields)]
 //use vstd::std_specs::result;
 use vstd::prelude::*;
+use vstd::seq::Seq;
+
+
 verus! {
 
 /* enum Result<A> {
@@ -48,12 +51,42 @@ impl<A> Result<A> {
     }
   } */
 
-  enum Const {
-    Atom { val : String },
-    Nat { i : u16 }, 
-    Str { s : String },
-    List { l : Vec<Const>} // vector???
+pub enum Const {
+  Atom (String),
+  Nat (u64), 
+  Str (String),
+  List (Vec<Const>) // vector???
+  } 
+
+/* pub enum SpecConst {
+  Atom (String),
+  Nat (u64), 
+  Str (String),
+  List (Seq<SpecConst>)
+  } */
+
+/* impl DeepView for Const {
+  type V = SpecConst;
+
+  open spec fn deep_view(&self) -> Self::V {
+    match self {
+      Const::Atom(s) => SpecConst::Atom(*s),
+      Const::Nat(n) => SpecConst::Nat(*n),
+      Const::Str(s) => SpecConst::Str(*s),
+      Const::List(vec) => SpecConst::List(vec.deep_view()),
+        
+        /* let mut seq = Seq::<SpecConst>::empty();
+        for i in 0..vec.len() {
+            seq = seq.push(vec[i as int].deep_view());
+        }
+
+        SpecConst::List(seq) */
+    
+      
+    }
   }
+} */
+  
   
 
   fn main(){
